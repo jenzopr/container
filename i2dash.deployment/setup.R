d2e226c210fb66b210dffe01dc6c13504073b1fc
Jens Preussner <jens.preussner@mpi-bn.mpg.de>
2019-10-29 14:39:11 +0100

    Added container draft for i2dash development

---
+++ i2dash.deployment/setup.R
@@ -0,0 +1,21 @@
+# Lock Bioc version
+bioc_version = "3.9"
+
+# Retrieve/provide tools --------------------------------------------------
+# Provide the convenient "pacman" package management kit
+if (!require(pacman, quietly = TRUE)) install.packages("pacman",
+                                                       suppressUpdates = TRUE, type = "source") 
+
+# Remove potential old Bioconductor version (ignored if not present)
+pacman::p_delete(char = c("BioInstaller", "BiocManager"), quiet = TRUE)
+
+# Fetch tools required here
+pacman::p_load(char = c("BiocManager", "git2r", "magrittr", "remotes", "flexdashboard", "rprojroot", "tidyr", "assertive"),
+               update = FALSE)
+
+# Update everything (already present) -------------------------------------
+BiocManager::install(checkBuilt = TRUE, ask = FALSE, version = bioc_version)
+
+# Install github dependencies ------------------------------------------------
+install_git(url = "https://gitlab.gwdg.de/loosolab/software/i2dash.git", repos = BiocManager::repositories(), dependencies = TRUE)
+install_git(url = "https://gitlab.gwdg.de/loosolab/software/i2dash.scrnaseq.git", repos = BiocManager::repositories(), dependencies = TRUE)
